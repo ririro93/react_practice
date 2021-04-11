34a83829515e67928e3c0ec8cdc1b735ba98a896
문준하 <ririro93@gmail.com>
2021-04-11 20:52:49 +0900

    Feat: tic-tac-toe extra features

--- README.md
+++ README.md
@@ -4,12 +4,24 @@
 ## 1. tic-tac-toe
 > the official react page tutorial
 
-- [x] 0. finish building the game, no extras
+- [x] 0. finish building the game following the tutorial
 - [x] 1. Display the location for each move in the format (col, row) in the move history list.
     - each square is now recognized by [i, j] not a single number from 0-8
-    - to deep copy square values for rendering,  tried using **lodash** library's `_.cloneMethod(<original arr>)` method
-- [ ] 2. Bold the currently selected item in the move list.
-- [ ] 3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
-- [ ] 4. Add a toggle button that lets you sort the moves in either ascending or descending order.
-- [ ] 5. When someone wins, highlight the three squares that caused the win.
-- [ ] 6. When no one wins, display a message about the result being a draw.
+    - to deep copy square 2d arrays' values for rendering,  tried using **lodash** library's `_.cloneMethod(<original arr>)` method
+- [x] 2. Bold the currently selected item in the move list.
+    - added className using ternary statement
+- [x] 3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+    - used 2 loops to first fill rows with 3 squares then a board with 3 rows
+    - -> code is hard to understand, there is probably an easier way
+- [x] 4. Add a toggle button that lets you sort the moves in either ascending or descending order.
+    -  added a boolean to Game state that can be clicked to be toggled
+    - -> if this state `ascending = false` => the array moves is reversed
+    - -> the ol tag containing the moves list is added the attribute reversed
+- [x] 5. When someone wins, highlight the three squares that caused the win.
+    - gave winning moves to Board as props
+    - -> if a squares coords was included in winning moves added className blue to highlight
+    - -> used lodash isEqual method to compare square coords and winning moves
+- [x] 6. When no one wins, display a message about the result being a draw.
+    - checked if stepNumber == 9 and changed to status to display Draw
+- [x] extra. add a reset game button that shows when there is a winner or the game is a draw
+    - set all the states to the initial values
--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -5,8 +5,13 @@ import './index.css';
 var _ = require('lodash');
 
 function Square(props) {
+  let className = "square";
+  if (props.isWinningMove) {
+    className += " blue";
+  }
+
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={className} onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -14,51 +19,70 @@ function Square(props) {
  
 class Board extends React.Component {
   renderSquare(i, j) {
+    const len = this.props.winningMoves.length;
+    let isWinningMove = false;
+    for (let k=0; k<len; k++) {
+
+      if (_.isEqual(this.props.winningMoves[k], [i,j])) {
+        isWinningMove = true
+      }
+    }
     return (
       <Square 
         value={this.props.squares[i][j]}
+        isWinningMove={isWinningMove}
         onClick={() => this.props.onClick(i, j)}
       />
     );
   }
 
   render() {
+    let board = [];
+    for (let i=0; i<3; i++) {
+      let rows = [];
+      for (let j=0; j<3; j++) {
+        // div 없이 그냥 내용만 넣고 싶은데 부모가 없어서 안되는듯?
+        rows.push(<div key={[i, j]}> {this.renderSquare(i, j)} </div>)
+      }
+      board.push(<div key={i} className="board-row">{rows}</div>)
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0, 0)}
-          {this.renderSquare(0, 1)}
-          {this.renderSquare(0, 2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(1, 0)}
-          {this.renderSquare(1, 1)}
-          {this.renderSquare(1, 2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(2, 0)}
-          {this.renderSquare(2, 1)}
-          {this.renderSquare(2, 2)}
-        </div>
+         {board}
       </div>
     );
   }
 }
 
 class Game extends React.Component {
+  initialState = {
+    history: [{
+      squares: Array.from(Array(3), () => Array(3).fill(null)),
+    }],
+    marked: ['start'],
+    stepNumber: 0,
+    xIsNext: true,
+    ascending: true,
+  }
+
   constructor(props) {
     super(props);
-    this.state = {
-      history: [{
-        squares: Array.from(Array(3), () => Array(3).fill(null))
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-    };
+    this.state = this.initialState;
+  }
+
+  handleReset() {
+    this.setState(this.initialState);
+  }
+
+  handleToggle() {
+    this.setState({
+      ascending: !this.state.ascending
+    });
   }
 
   handleClick(i, j) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const marked = this.state.marked.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = _.cloneDeep(current.squares);
     if (calculateWinner(squares) || squares[i][j]) {
@@ -69,6 +93,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      marked: marked.concat([[i, j]]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
@@ -84,39 +109,98 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    console.log('stepNumber: ', this.state.stepNumber)
-    console.log('current: ', current)
-    const winner = calculateWinner(current.squares);
+    const marked = this.state.marked;
+    let win = calculateWinner(current.squares);
+    let winner, winningMoves;
 
+    // get winner and winning moves
+    if (win) {
+      [winner, winningMoves] = win;
+    } else {
+      winner = win
+      winningMoves = []
+    }
+
+    // moves 
     const moves = history.map((step, move) => {
       const desc = move ?
         'Go to move #' + move :
         'Go to game start';
+      const nextMove = move ?
+        (move % 2) ?
+          'Player X marked ' + marked[move]:
+          'Player O marked ' + marked[move]:
+        "Player X's turn";
+
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button 
+            onClick={() => this.jumpTo(move)}
+            className={this.state.stepNumber === move ? "blue" : "black"}
+          >
+            {desc}
+          </button>
+          <div>
+            player {nextMove}
+          </div>
         </li>
       );
     });
+    if (!this.state.ascending) {
+      moves.reverse();
+    }
 
+    // status
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = (
+        <div>
+          <div>
+            Winner: {winner}
+          </div>
+          <button
+            onClick={() => this.handleReset()}
+          >
+            Restart Game
+          </button>
+        </div>
+      );
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-    
+
+    // when game over without winner
+    if (this.state.stepNumber === 9) {
+      status = (
+        <div>
+          <div>
+            Draw
+          </div>
+          <button
+            onClick={() => this.handleReset()}
+          >
+            Restart Game
+          </button>
+        </div>
+      );
+    }
+
+    // Ascending, Descending toggle button
+    console.log("ascending:", this.state.ascending);
+
     return (
       <div className="game">
         <div className="game-board">
           <Board 
             squares={current.squares}
+            winningMoves={winningMoves}
             onClick={(i, j) => this.handleClick(i, j)}
           />
         </div>
         <div className="game-info">
+          <button onClick={() => this.handleToggle()}>{this.state.ascending ? "Ascending" : "Descending"}</button>
           <div>{status}</div>
-          <ol>{moves}</ol>
+          <ol reversed={!this.state.ascending}>{moves}</ol>
         </div>
       </div>
     );
@@ -138,7 +222,7 @@ function calculateWinner(squares) {
   for (let i=0; i<lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a[0]][a[1]] && squares[a[0]][a[1]] === squares[b[0]][b[1]] && squares[a[0]][a[1]] === squares[c[0]][c[1]]) {
-      return squares[a[0]][a[1]];
+      return [squares[a[0]][a[1]], lines[i]];
     }
   }
   return null;
